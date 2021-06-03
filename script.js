8c9f15343a9cd8666233fea85d80bdbc6134c5b9
unknown <marinatwice82@gmail.com>
2021-06-03 16:14:48 +0300

    Highlight the active key

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 const piano = document.querySelector('.piano');
 const pianoКeys = document.querySelectorAll('.piano-key');
 
-
-//piano.addEventListener('click', (event) => playAudio(event));
+/*func playAudio*/
 function playAudio(src) {
     const audio = new Audio();
     audio.src = src;
     audio.currentTime = 0;
     audio.play();
   }
-  
+
 piano.addEventListener('click', (event) => {
     if(event.target.classList.contains('piano-key')) {
-        console.log("key");
-      const note = event.target.dataset.note;
-      console.log("note", note);
-      const src = `./assets/audio/${note}.mp3`;
-      console.log("src", src);
-      console.log("playAudio", playAudio(src));
-
-      playAudio(src);
+        const note = event.target.dataset.note;
+        const src = `./assets/audio/${note}.mp3`;
+        playAudio(src);
     }   
+});
+/*func playAudio*/  
+
+piano.addEventListener('click', (event) => {
+    if(event.target.classList.contains('piano-key')) {
+        pianoКeys.forEach((el) => {
+        if(el.classList.contains('active')) {
+            el.classList.remove('active');
+        }
+    });
+        event.target.classList.add('active');
+    }
   });
 
 
