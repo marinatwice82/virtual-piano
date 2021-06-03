dabb0397907c30b9c2c2516f6bbc9e1d5e059755
unknown <marinatwice82@gmail.com>
2021-06-03 20:22:59 +0300

    add func keydown

--- script.js
+++ script.js
@@ -21,16 +21,23 @@ piano.addEventListener('click', (event) => {
 piano.addEventListener('click', (event) => {
     if(event.target.classList.contains('piano-key')) {
         pianoКeys.forEach((el) => {
-        if(el.classList.contains('active')) {
-            el.classList.remove('active');
-        }
-    });
+            if(el.classList.contains('active')) {
+                el.classList.remove('active');
+            }
+        });
         event.target.classList.add('active');
     }
 });
 
 const btnContainer = document.querySelector('.btn-container');
 const btn = document.querySelectorAll('.btn');
+
+//const dataLetter = pianoКeys[0].getAttribute('data-letter');
+//console.log(dataLetter);
+//const dataNote = pianoКeys[0].getAttribute('data-note');
+//console.log(dataNote);
+const btnLetters = document.querySelector('.btn-letters');
+const btnNotes = document.querySelector('.btn-notes');
 
 btnContainer.addEventListener('click', (event) => {
     if(event.target.classList.contains('btn')) {
@@ -39,9 +46,42 @@ btnContainer.addEventListener('click', (event) => {
                 el.classList.remove('btn-active');
             }
         });
-        event.target.classList.add('btn-active');
+        event.target.classList.add('btn-active');  
     }
-
 });
 
+/*func letters*/
+btnLetters.addEventListener('click', (event) => {
+    pianoКeys.forEach(key => {
+        key.classList.add('letter');
+    });
+    btnNotes.classList.remove('btn-active')
+    btnLetters.classList.add('btn-active')
+});
+
+/*func notes*/
+btnNotes.addEventListener('click', (event) => {
+    pianoКeys.forEach(key => {
+        key.classList.remove('letter');
+    });
+    btnLetters.classList.remove('btn-active')
+    btnNotes.classList.add('btn-active')
+});
+
+/*func keydown*/
+window.addEventListener('keydown', (event) => {
+    pianoКeys.forEach(elem => {
+        if (`Key${elem.dataset.letter}` === event.code) {
+            console.log(elem.dataset.note)
+            elem.classList.add('piano-key-active');
+            elem.classList.remove('piano-key-active');
+            const note = elem.dataset.note;
+            const src = `assets/audio/${note}.mp3`;
+            playAudio(src);
+        };
+        elem.addEventListener('keyup', event => {
+            elem.classList.remove('piano-key-active');
+        });
+    });
+});
 
